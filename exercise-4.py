a2a2b66bcc065e85e594e3620ff4f26f389eb20f
Molinnaael <2025428el@aupp.edu.kh>
2025-07-07 22:25:25 +0700

    Group Homework Submission

--- exercise-4.py
+++ exercise-4.py
@@ -1,2 +1,11 @@
 def chunking_by(numbers, chunck):
     ...
+    split_list = []
+    while numbers:
+        split_list.append(numbers[:chunck]) 
+        numbers = numbers[chunck:] 
+    return split_list
+
+assert chunking_by([5, 4, 7, 3, 4, 5, 4], 3) == [[5, 4, 7], [3, 4, 5], [4]], "Test failed"
+assert chunking_by([3, 4, 5], 1) == [[3], [4], [5]], "Test failed"
+assert chunking_by([], 3) == [], "Test failed"
