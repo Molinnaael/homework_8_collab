8f4d6f298d034257e33b6b51b7c86502c423545a
Sokchanpich_Khlok <2025333khlok@aupp.edu.kh>
2025-07-07 22:19:51 +0700

    exercise-3

--- exercise-3.py
+++ exercise-3.py
@@ -1,2 +1,9 @@
 def remove_all_after(numbers, n):
-    ...
+    if not numbers:
+        return numbers # If the list is empty, just return the empty list
+    if n not in numbers:
+        return numbers # If n is not in the list, just return the original list
+    for i in range(len(numbers)):
+        if numbers[i] == n:
+        # If we find n, return the list from the beginning up to and includding n
+            return numbers[:i + 1]
