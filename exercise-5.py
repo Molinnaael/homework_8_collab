ef380ad93c3f0b53886b5699a084b1852c777c16
Tereza-2025310 <2025310rim@aupp.edu.kh>
2025-07-07 23:20:20 +0700

    update

--- exercise-5.py
+++ exercise-5.py
@@ -1,2 +1,15 @@
 def reverse_ascending(numbers):
-    ...
+    if not numbers:
+        return []
+    
+    result = []
+    current = [numbers[0]]
+    for i in range(1,len(numbers)):
+        if numbers [i] > numbers[i-1]:
+            current.append(numbers[i])
+        else:
+            result.extend(reversed(current))
+            current = [numbers[i]]
+
+    result.extend(reversed(current))
+    return result
