8cb21797cd5c51fd8d395dd8a6be19a8561e695d
Narirath202581 <2025081chhoeung@aupp.edu.kh>
2025-07-07 22:37:36 +0700

    Update exercise-1.py

--- exercise-1.py
+++ exercise-1.py
@@ -1,2 +1,10 @@
 def replace_last(numbers):
     ...
+    if len (numbers) <= 1: 
+        return numbers
+    return [numbers[-1]]+ numbers [:-1]
+
+print(replace_last([2, 3, 4, 1]))  
+print(replace_last([1, 2, 3, 4]))  
+print(replace_last([1]))        
+print(replace_last([]))
